8d46b9f0afc945ac0b33f59eac6409a7179378bc
polmes <pol.developer1@gmail.com>
2017-03-06 23:05:10 +0100

    Add server-side files

---
+++ buscar.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Comissió Verda</title>
+</head>
+
+<?php
+
+foreach ($variable as $key => $value) {
+	
+}
+
+?>
+
+<body>
+	<h1>Test</h1>
+	<label for="comarca"></label>
+	<select id="comarca" name="comarca">
+		<option value="La Vall d’Aran">La Vall d’Aran</option>
+		<option value="Alta Ribagorça">Alta Ribagorça</option>
+		<option value="Pallars Sobirà">Pallars Sobirà</option>
+		<option value="Pallars Jussà">Pallars Jussà</option>
+		<option value="Alt Urgell">Alt Urgell</option>
+		<option value="Cerdanya">Cerdanya</option>
+		<option value="Berguedà">Berguedà</option>
+		<option value="Solsonès">Solsonès</option>
+		<option value="Ripollès">Ripollès</option>
+		<option value="Garrotxa">Garrotxa</option>
+		<option value="Alt Empordà">Alt Empordà</option>
+		<option value="Baix Empordà ">Baix Empordà </option>
+		<option value="La Selva">La Selva</option>
+		<option value="Maresme">Maresme</option>
+		<option value="Barcelonès">Barcelonès</option>
+		<option value="Baix Llobregat">Baix Llobregat</option>
+		<option value="Garraf ">Garraf </option>
+		<option value="Baix Penedès">Baix Penedès</option>
+		<option value="Tarragonès">Tarragonès</option>
+		<option value="Baix Camp">Baix Camp</option>
+		<option value="Baix Ebre">Baix Ebre</option>
+		<option value="Montsià">Montsià</option>
+		<option value="Pla de l’Estany">Pla de l’Estany</option>
+		<option value="Gironès">Gironès</option>
+		<option value="Vallès Oriental">Vallès Oriental</option>
+		<option value="Vallès Occidental">Vallès Occidental</option>
+		<option value="Alt Camp">Alt Camp</option>
+		<option value="Alt Penedès">Alt Penedès</option>
+		<option value="Noguera">Noguera</option>
+		<option value="Segrià">Segrià</option>
+		<option value="Pla d’Urgell">Pla d’Urgell</option>
+		<option value="Urgell">Urgell</option>
+		<option value="Segarra">Segarra</option>
+		<option value="Anoia">Anoia</option>
+		<option value="Garrigues">Garrigues</option>
+		<option value="Conca de Barberà">Conca de Barberà</option>
+		<option value="Bages">Bages</option>
+		<option value="Osona">Osona</option>
+		<option value="Terra Alta">Terra Alta</option>
+		<option value="Ribera d’Ebre">Ribera d’Ebre</option>
+		<option value="Priorat">Priorat</option>
+	</select>
+</body>
+
+</html>
---
+++ oferir.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+
+<body>
+	<label for="comentaris">Comentaris</label>
+	<textarea id="comentaris" type="text"></textarea>
+</body>
+
+</html>
